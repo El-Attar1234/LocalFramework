45b5568ae14257e60e5eb254eb2c42e4a4ba1904
Mahmoud El-attar <mahmoudelattar427@gmail.com>
2023-05-02 09:12:28 +0300

    add framework

---
+++ OwnFramework/OwnFramework/MainService.swift
@@ -0,0 +1,17 @@
+//
+//  MainService.swift
+//  OwnFramework
+//
+//  Created by Ibtikar on 02/05/2023.
+//
+
+import Foundation
+
+public class MainService {
+    
+    private init(){}
+    
+    public func doJob() -> String {
+        return "I'm doing my job now"
+    }
+}
---
+++ OwnFramework/OwnFramework/OwnFramework.h
@@ -0,0 +1,18 @@
+//
+//  OwnFramework.h
+//  OwnFramework
+//
+//  Created by Ibtikar on 02/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OwnFramework.
+FOUNDATION_EXPORT double OwnFrameworkVersionNumber;
+
+//! Project version string for OwnFramework.
+FOUNDATION_EXPORT const unsigned char OwnFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OwnFramework/PublicHeader.h>
+
+
